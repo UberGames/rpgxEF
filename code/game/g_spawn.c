e416f650eaa57a6c9935d224f6354b1f04d029ba
Harry Young <hendrik.gerritzen@googlemail.com>
2012-11-19 19:28:49 +0100

    Fix works so I discontinued and removed the borg elevator an the lua debug file I'm going to make a release of this version for CB now
    Signed-off-by: Harry Young <hendrik.gerritzen@googlemail.com>

--- code/game/g_spawn.c
+++ code/game/g_spawn.c
@@ -143,7 +143,6 @@ void SP_func_brushmodel(gentity_t *ent); // for brushmodel hijacking :D
 void SP_func_lightchange(gentity_t *ent); // "toggling" light
 void SP_func_targetmover(gentity_t *ent);
 void SP_func_stasis_door(gentity_t *ent);
-void SP_func_borg_elevator(gentity_t *ent);
 
 void SP_trigger_always (gentity_t *ent);
 void SP_trigger_multiple (gentity_t *ent);
@@ -325,7 +324,6 @@ spawn_t	spawns[] = {
 	{"func_lightchange",			SP_func_lightchange},
 	{"func_targetmover",			SP_func_targetmover},
 	{"func_stasis_door",			SP_func_stasis_door},
-	{"func_borg_elevator",			SP_func_borg_elevator},
 
 	// Triggers are brush objects that cause an effect when contacted
 	// by a living player, usually involving firing targets.
