3d4ee1c97e0ad6075e271ef868cbe8dd6090b5ac
Walter Julius Hennecke <walter.julius@hennecke-online.net>
2012-11-30 12:29:10 +0100

    Update entityDefParser and added luaDefParser (WIP)
    * README of entityDefParser now contains build instructions for linux,
    mac, and windows
    * added first version of luaDefParser (for now it just lexes the file
    and outputs what it found. In future I want it to output a lyx file)

---
+++ code/tools/luaDefParser/test.c
@@ -0,0 +1,16 @@
+/* luamodule test */
+
+int a;
+
+/* test */
+void test() {
+
+}
+
+/*
+ * \function test.func(float a, entity b);
+ * \param float a "this is a";
+ * \desc test 123;
+ * \desc "test
+ * 			test2";
+ */
