14f7daa52294345f063458e963fc18397698b876
Walter Julius Hennecke <walter.julius@hennecke-online.net>
2013-07-08 21:08:45 +0200

    added missing include in g_cinematic.h ...
    wow that must have been missing for ages

--- code/game/g_cinematic.h
+++ code/game/g_cinematic.h
@@ -3,6 +3,8 @@
 #ifndef _G_CINEMATIC_H
 #define _G_CINEMATIC_H
 
+#include "g_local.h"
+
 void Cinematic_ActivateCameraMode(gentity_t *ent, gentity_t *target);
 void Cinematic_DeactivateCameraMode(gentity_t *ent);
 void Cinematic_ActivateGlobalCameraMode(gentity_t *target);
