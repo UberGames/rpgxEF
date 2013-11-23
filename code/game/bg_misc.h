a9cc86d1d8e216148652e9009294425a6ae43bd2
Walter Julius Hennecke <walter.julius@hennecke-online.net>
2013-11-23 23:04:21 +0100

    Added ray bounding box interception test.

--- code/game/bg_misc.h
+++ code/game/bg_misc.h
@@ -1,7 +1,37 @@
 #ifndef _BG_MISC_H
 #define _BG_MISC_H
 
+#include "q_shared.h"
+
 void BG_LanguageFilename(char *baseName,char *baseExtension,char *finalName);
 char* BG_RegisterRace( const char *name );
+
+typedef struct bgRay_s bgRay_t;
+struct bgRay_s {
+	vec3_t origin;
+	vec3_t direction;
+	vec3_t inverse_direction;
+	int sign[3];
+};
+
+/**
+ * @brief Prepares a ray with a given origin and direction.
+ * @param ray The ray.
+ * @param origin The origin.
+ * @param dir The direction.
+ */
+void BG_PrepareRay(bgRay_t* ray, vec3_t origin, vec3_t dir);
+
+typedef struct bgBBox_s bgBBox_t;
+struct bgBBox_s {
+	vec3_t bounds[2];
+};
+
+/**
+ * @brief Checks is a given ray intersects with a given bounding box.
+ * @param ray The ray.
+ * @param bbox The bounding box.
+ */
+int BG_RayIntersect(bgRay_t* ray, bgBBox_t* bbox);
 
 #endif /* _BG_MISC_H */
